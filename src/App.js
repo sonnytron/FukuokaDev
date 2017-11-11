ae00f9843001dd29df92546558ea56f3f0cf91a6
Sonny <sonny@Sonnys-iMac.local>
2017-11-11 18:41:04 +0900

    Added SPA Files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <HashRouter>
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path = "/" component={Home}/>
+            <Route path = "/stuff" component={Stuff}/>
+            <Route path = "/contact" component={Contact}/>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>HELLO!</h2>
+                <p>
+                    This will be some sample data to fill the page for the Home section. Please ignore it. 
+                    Tabs and new lines don't do much in the page, but they make it easier to write in the editor. 
+                    Lately I've been using Visual Studio Code and wow, what a powerful editor. 
+                </p>
+                <p>
+                    Learning front end development has been much more enjoyable after spending two yeasr building iOS applications. 
+                </p>
+            </div>
+        );
+    }
+}
+export default Home
