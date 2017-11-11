0c74582e5cd4e9ce46c261802409f7261fa86aba
Sonny <sonny@Sonnys-iMac.local>
2017-11-11 17:48:35 +0900

    Changed index

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Simple SPA</h1>
+        <ul className="header">
+          <li><a href="/">Home</a></li>
+          <li><a href="/stuff">Stuff</a></li>
+          <li><a href="/contact">Contact</a></li>  
+        </ul>
+        <div className="content">
+        
+        </div>
       </div>
     );
   }
